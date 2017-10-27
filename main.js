a3ee0360b27ef9ab4ece67f06d1074f872c49836
NGOLDWEEKLY <ngoldweekly@gmail.com>
2017-10-27 21:53:44 +0800

    a

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
- self.addEventListener('fetch', function(event) {
+var cacheName = 'v2';
+var cacheFiles = [
+'./',
+'./index.html'
+]
+
+self.addEventListener('fetch', function(event) {
   event.respondWith(new Response("Hello world!"));
 });
   
   self.addEventListener('install', function(event) {
   event.waitUntil(
-    fetchStuffAndInitDatabases()
+    caches.open(cacheName).then(function(cache){
+    console.log("[ServiceWorker] Caching cacheFiles");
+     return cache.addAll(cacheFiles);
+    
+    })
   );
 });
 
