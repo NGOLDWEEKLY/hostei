93db372795bbc649ed629d40b74aacdd710cd227
NGOLDWEEKLY <ngoldweekly@gmail.com>
2018-12-19 14:33:06 +0800

    gf

--- s1s/user/index.php
+++ s1s/user/index.php
@@ -1,5 +1,4 @@
-﻿
-    <?php
+<body><?php
 $u = $_REQUEST["user_id"];
 $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567891011121314151617181920212223242526';
 $shuffled = str_shuffle($str);
@@ -7,8 +6,8 @@ $shuffled = str_shuffle($str);
 
 $str1 = substr($shuffled,1,30);
 $str2 = substr_replace($str1, 'a', -22, -20);
-
-echo $str2;
+ $strn = substr($str2, -21, 1); 
+echo $strn;
 
 ?>
-<body><style></style></body>
+<style></style>hello</body>
